c342451826dbc3a2e7d21e1f8f668567d173d3e3
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-11-20 15:28:23 +0100

    `Add` NavItems assets

---
+++ assets/NavItems.js
@@ -0,0 +1,20 @@
+const NavItems = [
+    {
+        name: "Home",
+        path: "/"
+    },
+    {
+        name: "About",
+        path: "/about"
+    },
+    {
+        name: "Projects",
+        path: "/projects"
+    },
+    {
+        name: "Links",
+        path: "/links"
+    }
+];
+
+export default NavItems;
