50649cf021f639c3c8d123f726bdde5b68dc2810
Fyxren <68126277+Fyxren@users.noreply.github.com>
2022-11-20 12:15:28 +0100

    `Add` ThemeProvider & Lucide Icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Head>
+        <meta name='keywords' content='fyxren portfolio personal' />
+        <meta name='language' content='EN' />
+        <meta name='robots' content='index,follow' />
+        <meta name='author' content='Fyxren' />
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
