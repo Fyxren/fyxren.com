29e7e9f8f8266639a03a949e0f0b9cad2220b32d
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-01-16 14:21:23 +0100

    `Set` light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        logo: {
+          start: '#0386FF',
+          end: '#FF00CC'
+        }
+      }
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "#111111",
+          secondary: "#333333",
+          accent: "#7654FF",
+          "base-100": "#F9F9F9",
+          "base-200": "#FFFFFF"
+        },
+      },
+    ]
+  }
 };
