fdac96a5b3c92bbfa0e918d8a1f9cf1582a84f1d
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-01-16 19:20:25 +0100

    `Update - About` first card padding

--- pages/about.js
+++ pages/about.js
@@ -21,7 +21,7 @@ export default function About() {
                         <Heart />
                         Love ya
                     </span>
-                    <div className="card-body">
+                    <div className="py-5 px-5">
                         <h2 className="card-title">Ben // Fyxren (18)</h2>
                         <p>Professional idiot, coder, gamer.</p>
                         <div className="flex flex-col gap-2 mt-5">
