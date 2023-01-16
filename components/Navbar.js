cadc946efbe9e630401629a2b4fd245bc1afbdcd
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-01-16 16:20:44 +0100

    `Fix - Navbar` capitalize theme items

--- components/Navbar.js
+++ components/Navbar.js
@@ -75,7 +75,7 @@ const Navbar = () => {
                                         <div data-theme={val} className="bg-base-100 text-base-content w-full cursor-pointer font-sans">
                                             <div className="grid grid-cols-5 grid-rows-3">
                                                 <div className="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4">
-                                                    <div className="flex-grow text-sm font-bold">
+                                                    <div className="flex-grow text-sm font-bold capitalize">
                                                         {val}
                                                     </div>
                                                     <div className="flex flex-shrink-0 flex-wrap gap-1">
