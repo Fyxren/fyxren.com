fa6d5696251f83e7b8d438ae90bfc518dbf0c111
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-01-17 21:25:09 +0100

    `Fix - Home` image hover only on lg & xl

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 
       <main className="flex justify-center h-full p-5 -mt-20 text-center md:mt-auto lg:text-left lg:p-20">
         <div className="flex-col gap-20 hero-content lg:gap-40 lg:flex-row-reverse">
-          <img src="/images/logo.png" className="w-40 duration-300 rounded-lg shadow-2xl md:w-52 lg:w-64 hover:-translate-y-10 hover:rotate-3 hover:scale-125" />
+          <img src="/images/logo.png" className="w-40 duration-300 rounded-lg shadow-2xl md:w-52 lg:w-64 lg:hover:-translate-y-10 lg:hover:rotate-3 lg:hover:scale-125" />
           <div>
             <h1 className="text-4xl lg:text-6xl font-poly flex-nowrap">Greetings, I'm <span className="font-bold">Fyxren</span>👋</h1>
             <p className="py-6 text-lg lg:text-2xl">Professional idiot, coder, gamer.</p>
