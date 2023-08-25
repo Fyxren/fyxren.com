dc7ef1ecf3a8c9eb2972f75f097c50a048dab267
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-08-25 18:11:11 +0200

    `Add` Header

---
+++ src/lib/components/elements/Header.svelte
@@ -0,0 +1,12 @@
+<div class="font-rubik mt-40 flex items-center justify-between">
+	<div>
+		<p class="text-5xl">Greetings! I'm</p>
+		<h1 class="text-9xl font-bold tracking-wider">Fyxren</h1>
+		<p class="font-thin italic text-2xl mt-5">Smart Industry student, hobbyist coder and gamer.</p>
+	</div>
+	<img
+		src="https://cdn.discordapp.com/avatars/462914535351779328/d5408b3d8c464400f6f6b3c8747a4630.png?size=4096"
+		alt="Fyxren's avatar"
+		class="w-80 rounded-lg"
+	/>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="px-52">
+	<slot />
+</div>
