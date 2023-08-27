09ac36a4fbf9bda61a7246ba7f491f6736519043
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-08-27 20:11:32 +0200

    `Add` meta

---
+++ src/lib/util/meta.ts
@@ -0,0 +1,10 @@
+export default {
+    title: 'Fyxren - Smart Industry student, hobbyist coder and gamer.',
+    description: 'Greetings, welcome to my little corner on the internet. Feel free to take a look and learn more about me!',
+    url: 'https://fyxren.com',
+    image: 'https://i.postimg.cc/9QwmpnJQ/D-Fyxren-Banner.png',
+    author: 'Fyxren',
+    siteName: 'Fyxren',
+    keywords: ['fyxren', 'fyx', 'fy', 'fyxren.com', 'ben', 'ben van der weerd'],
+    themeColor: '#000000'
+}
