c91c3c65213f21504611417d41a89f8eceef3369
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-08-29 10:40:00 +0200

    `Add - Types` Lanyard

--- src/lib/util/global.interface.ts
+++ src/lib/util/global.interface.ts
@@ -18,3 +18,68 @@ export interface IError {
     header: string;
     message: string;
 }
+
+// Lanyard
+
+export interface ILanyardResponse {
+    success: boolean;
+    data: ILanyardData;
+}
+
+export interface ILanyardData {
+    spotify: ISpotify;
+    listening_to_spotify: boolean;
+    kv: any;
+    discord_user: IDiscordUser;
+    discord_status: string;
+    activities: IActivity[];
+    active_on_discord_mobile: boolean;
+    active_on_discord_desktop: boolean;
+    active_on_discord_web: boolean;
+}
+
+export interface IAssets {
+    large_image: string;
+    large_text: string;
+    small_image?: string;
+    small_text?: string;
+}
+
+export interface ITimestamps {
+    start: number;
+    end?: number;
+}
+
+export interface IDiscordUser {
+    id: string;
+    username: string;
+    global_name: string;
+    display_name: string;
+    public_flags: number;
+    avatar_decoration_data: any;
+}
+
+export interface ISpotify {
+    track_id: string;
+    timestamps: ITimestamps
+    album: string;
+    album_art_url: string;
+    artist: string
+    song: string;
+}
+
+export interface IActivity {
+    flags?: number;
+    id: string;
+    name: string;
+    type: number;
+    state: string;
+    session_id?: string;
+    details: string;
+    timestamps: ITimestamps,
+    assets: IAssets;
+    buttons: any;
+    sync_id?: string;
+    created_at: number;
+    party: any;
+}
