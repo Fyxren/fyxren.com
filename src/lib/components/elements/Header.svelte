61638a69eddce732e2612575d9ef180e682d998e
Fyxren <68126277+Fyxren@users.noreply.github.com>
2023-09-17 14:01:15 +0200

    `Fix` header "Fyxren" not centered on mobile

--- src/lib/components/elements/Header.svelte
+++ src/lib/components/elements/Header.svelte
@@ -7,8 +7,8 @@
 >
 	<div class="mt-5 text-center lg:text-left lg:mt-auto">
 		<p class="text-2xl lg:text-5xl">Greetings! I'm</p>
-		<h1 class="text-6xl font-bold tracking-wider lg:text-9xl gradient-text w-fit">Fyxren</h1>
-		<p class="mt-5 text-xl italic font-thin lg:text-2xl">
+		<h1 class="mb-5 text-6xl font-bold tracking-wider lg:text-9xl gradient-text">Fyxren</h1>
+		<p class="text-xl italic font-thin lg:text-2xl">
 			Smart Industry student, hobbyist coder and gamer.
 		</p>
 	</div>
