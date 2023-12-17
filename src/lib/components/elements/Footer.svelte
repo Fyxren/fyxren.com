f9576c5e4ca9aa0965e168c9ed85767b438f60e3
Fyxren <benweerd@gmail.com>
2023-12-17 22:18:28 +0100

    feat: add footer

---
+++ src/lib/components/elements/Footer.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	// @ts-expect-error
+	// eslint-disble-next-line no-undef
+	const pkg = __PACKAGE__;
+</script>
+
+<footer class="mb-10 space-y-8">
+	<hr class="!border-t-2" />
+	<div class="grid grid-cols-8 px-52">
+		<div class="col-span-2">(✿◠‿◠)</div>
+		<div class="col-span-4 place-self-center">
+			Hand-crafted with equal parts
+			<span class="text-primary-500">dedication</span>
+			and
+			<span class="text-primary-500">procrastination.</span>
+		</div>
+		<div class="col-span-2 place-self-end">
+			<a href="https://github.com/Fyxren/fyxren.com/releases">
+				{pkg.version}
+			</a>
+		</div>
+	</div>
+</footer>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const pkg = (require('./package.json'));
+
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	define: {
+		__PACKAGE__: JSON.stringify(pkg),
+	}
 });
