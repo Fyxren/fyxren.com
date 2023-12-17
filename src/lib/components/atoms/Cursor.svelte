af30d0a99369ff973ecfef0fbae0ba0c6751baef
Fyxren <benweerd@gmail.com>
2023-12-17 17:07:16 +0100

    fix: mouse cursor circle position aligned

--- src/lib/components/atoms/Cursor.svelte
+++ src/lib/components/atoms/Cursor.svelte
@@ -18,5 +18,5 @@
 
 <div
 	class="fixed top-0 left-0 z-50 w-10 h-10 border-2 border-primary-500/50 pointer-events-none transition-all ease-cursor rounded-full"
-	style="transform: translate({x - 20}px, {y - 20}px) scale({scale});"
+	style="transform: translate({x - 17}px, {y - 14}px) scale({scale});"
 />
