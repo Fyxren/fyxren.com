8b9ed7dab032c288052959cdc1a20030074a16be
Fyxren <benweerd@gmail.com>
2024-03-07 13:42:36 +0100

    feat: add meta tags

---
+++ src/lib/data/meta.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'Fyxren - Smart Industry student, hobbyist coder and gamer.',
+    description:
+        'Greetings, welcome to my little corner on the internet. Feel free to take a look and learn more about me!',
+    url: 'https://fyxren.com',
+    image: 'https://i.postimg.cc/Qtm907rS/OG.png',
+    author: 'Fyxren',
+    siteName: 'Fyxren',
+    keywords: ['fyxren', 'fyx', 'fy', 'fyxren.com', 'ben', 'ben van der weerd'],
+    themeColor: '#181B2B'
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,32 @@
 	import Header from '$lib/components/elements/Header.svelte';
 	import Links from '$lib/components/elements/Links.svelte';
 	import Projects from '$lib/components/elements/Projects.svelte';
+	import meta from '$lib/data/meta';
 </script>
 
 <svelte:head>
-	<title>Fyxren - Professional idiot, coder, gamer</title>
-	<meta name="description" content="Welcome to my little corner of the internet." />
-	
+	<!-- Primary Meta Tags -->
+	<title>{meta.title}</title>
+	<meta name="title" content={meta.title} />
+	<meta name="description" content={meta.description} />
+	<meta name="theme-color" content={meta.themeColor} />
+	<meta name="keywords" content={meta.keywords.join(',')} />
+	<meta name="author" content={meta.author} />
+
+	<!-- Open Graph -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={meta.url} />
+	<meta property="og:title" content={meta.title} />
+	<meta property="og:description" content={meta.description} />
+	<meta property="og:image" content={meta.image} />
+	<meta property="og:site_name" content={meta.siteName} />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content={meta.url} />
+	<meta property="twitter:title" content={meta.title} />
+	<meta property="twitter:description" content={meta.description} />
+	<meta property="twitter:image" content={meta.image} />
 </svelte:head>
 
 <main class="px-4 lg:px-20">
